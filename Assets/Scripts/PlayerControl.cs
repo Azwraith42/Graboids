22fb2c43ad9b1639318ea7f0dd1dd9cf04d39dc9
William Situ <wsitu@ucsc.edu>
2013-11-25 01:11:45 -0800

    added in movement for the ship

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
+	public float rotationalForce;
+	public float maxAngVel;
+	public float moveForce;
+	public float maxVel;
+	private string axisName = "Horizontal";
+
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +17,24 @@ public class PlayerControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		//rotate ship
+		rigidbody2D.AddTorque(Input.GetAxis(axisName)*-rotationalForce);
+
+		if (rigidbody2D.angularVelocity > maxAngVel)
+			rigidbody2D.angularVelocity = maxAngVel;
+		else if (rigidbody2D.angularVelocity < -maxAngVel)
+			rigidbody2D.angularVelocity = -maxAngVel;
+
+
+		//propel ship in the angle its facing
+		float xComponent = Mathf.Cos (transform.eulerAngles.z * Mathf.Deg2Rad) * moveForce;
+		float yComponent = Mathf.Sin (transform.eulerAngles.z * Mathf.Deg2Rad) * moveForce;
+		if (Input.GetKey(KeyCode.UpArrow))
+			rigidbody2D.AddForce (new Vector2 (xComponent, yComponent));
+
+		if (rigidbody2D.velocity.magnitude > maxVel)
+			rigidbody2D.velocity = rigidbody2D.velocity.normalized * maxVel;
+
 	}
 }
