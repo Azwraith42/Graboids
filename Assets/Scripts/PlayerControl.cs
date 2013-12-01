3f6e427e147a2c19b8b46b019b11a86819c32255
Alec Reeves <ahreeves@ucsc.edu>
2013-12-01 01:15:19 -0800

    tried to add screen wrap, will fix tomorrow

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour {
 
+	//public GameObject Ship;
 	public float rotationalForce;
 	public float maxAngVel;
 	public float moveForce;
@@ -10,6 +11,8 @@ public class PlayerControl : MonoBehaviour {
 	public float pullForce;
 	public float shootForce;
 	public float shootSpinForce;
+	public Vector3 wrap = new Vector3();
+	public Vector3 wrapMax = new Vector3();
 	private string axisName = "Horizontal";
 	private RaycastHit2D grabTarget;
 	private bool hasTarget = false;
@@ -20,6 +23,20 @@ public class PlayerControl : MonoBehaviour {
 	 *  ====================
 	 */
 
+
+	//awake to guarentee it happends first
+	void Awake(){
+//		var wrapmin:Vector3=Camera.main.ScreenToWorldPoint(Vector3(0,0,Camera.main.transform.position.y));
+//		wrap=Vector3(wrapmin.x-4,wrapmin.z-4);
+//		var wrapmax:Vector3=Camera.main.ScreenToWorldPoint(Vector3(Screen.width,Screen.height,Camera.main.transform.position.y));
+//		wrapMax=Vector3(wrapmax.x+4,wrapmax.z+4);
+
+		Vector3 wrapmin = Camera.main.ScreenToWorldPoint (new Vector3 (0, Camera.main.transform.position.y, 0));
+		wrap = new Vector3(wrapmin.x-4,wrapmin.y-4, 0);
+		Vector3 wrapmax = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height, 0)); //0 on this line changed from Camera.main.transform.position.y
+		wrapMax = new Vector3 (wrapmax.x + 4, wrapmax.y + 4, 0);
+	}
+
 	// Use this for initialization
 	void Start () {
 	}
@@ -27,6 +44,19 @@ public class PlayerControl : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		//screen wrap -------------  HOW THE FUCK DO POSITION CHANGE???
+		if (rigidbody2D.transform.position.x > wrapMax.x) {
+			rigidbody2D.transform.position.x = wrap.x;
+		}
+		if (rigidbody2D.transform.position.x < wrap.x) {
+			rigidbody2D.transform.position.x = wrapMax.x;
+		}
+		if (rigidbody2D.transform.position.y > wrapMax.y) {
+			rigidbody2D.transform.position.y = wrap.y;
+		}
+		if (rigidbody2D.transform.position.y < wrap.y) {
+			rigidbody2D.transform.position.y = wrapMax.y;
+		}
 
 		//rotate ship
 		rigidbody2D.AddTorque(Input.GetAxis(axisName)*-rotationalForce);
