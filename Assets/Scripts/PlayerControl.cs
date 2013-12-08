7a678fea110fff20c6e64e087c1760df5f2f9a32
Alec Reeves <ahreeves@ucsc.edu>
2013-12-07 18:30:23 -0800

    Plz upboat kthxbye

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -201,5 +201,6 @@ public class PlayerControl : MonoBehaviour {
 	void OnCollisionEnter2D(Collision2D coll) {
 		GUIscript.isDead = true;
 		Destroy(this.gameObject);
+		EventManager.instance.QueueEvent (new ShipDestroy ());
 	}
 }
