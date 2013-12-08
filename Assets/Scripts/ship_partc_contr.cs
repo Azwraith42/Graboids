dba89c9893092bbfe0477d8e5626af4a8c038ccc
William Situ <wsitu@ucsc.edu>
2013-12-08 13:54:59 -0800

    added particle system for ship movement
    particle system is a child to the ship
    has the ship_partc_contr script to turn it on/off
    ship_part_mat was used to make the particle look square/pixely

---
+++ Assets/Scripts/ship_partc_contr.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ship_partc_contr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		particleSystem.enableEmission = false;
+		if (Input.GetKey(KeyCode.UpArrow)) {
+			particleSystem.enableEmission = true;
+		}
+	}
+}
