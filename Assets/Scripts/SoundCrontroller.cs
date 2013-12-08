9b8fc5f6a0dc0ba04c9ca105f7d01da4d6163913
Alec Reeves <ahreeves@ucsc.edu>
2013-12-07 18:29:04 -0800

    added files
    Event handling and Sound manager

---
+++ Assets/Scripts/SoundCrontroller.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ShipDestroy : BaseEvent{}
+
+
+public class SoundCrontroller : MonoBehaviour, IEventListener {
+
+	public AudioSource[] CrashSources;
+
+	public bool HandleEvent(IEvent e){
+		return false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		EventManager.instance.AddListener (this, "ShipDestroy", this.onShipDestroy);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public bool onShipDestroy(IEvent e){
+		CrashSources [0].Play ();
+		return true;
+	}
+}
